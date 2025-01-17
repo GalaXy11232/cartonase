90457b39c0b066e69da9bf4b5cc3c2d06db95538
GalaXy <neamtumatei08@gmail.com>
2025-01-17 22:41:57 +0200

    sistem save-load empiric

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ const defaultJSONStructure = {
 }
 
 window.addEventListener("load", (ev) => {
+    // console.log(JSON.parse(localStorage.getItem(localStorageEntry))["raspunsuri"].includes(["1", " 2", " 3", " 4", " 1"]))
     checkLocalStorage(localStorageEntry, defaultJSONStructure);
     // updateData(localStorageEntry, "intrebari", "balls");
 })
 
 
 
-function checkEmpty(text, isList = false) {
+function checkEmpty(text) {
     // if (isList) {
     //     for (i in text) {
     //         let textArr = i.split("");
@@ -31,21 +32,32 @@ function checkEmpty(text, isList = false) {
 }
 
 function addInputData(inputIDs) {
-    // const inputQuestion = document.getElementById(inputIDs[0]);
-    // const inputAnswers = document.getElementById(inputIDs[1]);
+
     const inputQuestion = document.getElementById(inputIDs[0]).value;
-    const inputAnswers = document.getElementById(inputIDs[1]).value.split(" ");
+    const inputAnswerString = document.getElementById(inputIDs[1]).value;
+    const inputAnswers = inputAnswerString.split(",");
 
     console.log(inputAnswers);
     if (inputQuestion == "clear") { localStorage.clear(); return; }
+    // if (inputQuestion == "skib") { download(localStorage.getItem(localStorageEntry), 'intrebari.json'); return; }
 
     // Check for invalid input
-    if (inputQuestion.length == 0 || checkEmpty(inputQuestion)) {
+    if (
+        inputQuestion.length == 0 || inputAnswerString.length == 0 || inputAnswers.length == 0 ||
+        checkEmpty(inputAnswerString) || checkEmpty(inputQuestion)
+    ) {
         alert("Te rog introdu o intrebare valida.");
         return;
     }
 
-    updateData(localStorageEntry, "intrebari", inputQuestion);
+    if (isNaN(inputAnswers[4]) || Number(inputAnswers[4]) < 1 || Number(inputAnswers[4]) > 4) { 
+        alert ("Te rog alege un numar intre 1 si 4 pentru a determina varianta corecta!"); 
+        return; 
+    }
+
+
+    // Update localStorage
+    updateData(inputQuestion, inputAnswers);
 
 }
 
@@ -62,39 +74,67 @@ function checkLocalStorage(entry, value) {
     return true;
 }
 
-function updateData(data, key, value) {
-    let dataObj = JSON.parse(localStorage.getItem(data));
+function updateData(question, answers) {
+    let dataObj = JSON.parse(localStorage.getItem(localStorageEntry));
     
-    if(existsIn(dataObj, key, value)) { alert("exista deja intrebarea"); return; }
+    if (dataObj.intrebari.includes(question)) { 
+        alert("exista deja intrebarea"); 
+        return; 
+    }
 
-    dataObj[key].push(value);
-    console.log(dataObj);
 
-    localStorage.setItem(data, JSON.stringify(dataObj));
+    dataObj.intrebari.push(question);
+    dataObj.raspunsuri.push(answers);
 
-    // fetch json data
-        // fetch(jsonPath).then(
-        //     response => { return response.json(); }
-        // ).then(
-        //     data => {
-        //         console.log(data);
-        //         var cpy = data[key];
-        //         // console.log(data.intrebari, typeof(data.intrebari))
-        //         cpy.push(value);
-        //         console.log("cpu", cpy);
-        //     }
-        // )
+    localStorage.setItem(localStorageEntry, JSON.stringify(dataObj));
 
-        // fetch(jsonPath).then(
-        //     response => {return response.json();}
-        // ).then(
-        //     data => {
-        //         console.log(data);
-        //     }
-        // )
-    
+}
+
+function clearStorage() {
+    localStorage.clear();
+    localStorage.setItem(localStorageEntry, JSON.stringify(defaultJSONStructure));
+}
+
+
+// If equal arrays, return true
+function checkArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+
+    for (let i = 0; i < arr1.length; i++) 
+        if (arr1[i] !== arr2[i]) return false;
+
+    return true;
 }
 
 function existsIn(obj, key, value) {
     return obj[key].includes(value);
+}
+
+
+
+
+function loadfile() {
+    let file = document.getElementById("fileinput").files[0];
+    
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        
+        localStorage.setItem(localStorageEntry, contents)
+    };
+    reader.readAsText(file);
+}
+
+function savefile() {
+    let blob = new Blob([localStorage.getItem(localStorageEntry)], {type: "application/json"})
+    
+    let url = URL.createObjectURL(blob);
+    let a = document.createElement("a");
+    a.href = url;
+    a.download = "intrebari.json";
+    a.click();
+
+    clearStorage();
+    a.remove();
+    URL.revokeObjectURL(url);
 }
