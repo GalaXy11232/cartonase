3b54d1ea5f5cf6a84387dcbfb0e747ae285146b3
GalaXy <neamtumatei08@gmail.com>
2025-01-17 08:31:36 +0200

    script.js update (wip)

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-const data = require('./intrebari.json')
-console.log(data.intrebari);
+const jsonPath = "./intrebari.json"
 
-function returntext() {
-    const inputform = document.getElementById("typeinput");
-    alert(inputform.value);
+window.addEventListener("load", (ev) => {
+    updateJsonData("intrebari", "balls");
+})
+
+
+
+function checkEmpty(text) {
+    let textArr = text.split("");
+    for (i in textArr)
+        if (textArr[i] != " ") return false;
+    
+    return true;
+}
+
+function addQuestion() {
+    const inputForm = document.getElementById("typeinput");
+    const inputValue = inputForm.value;
+
+    // Check for invalid input
+    if (inputValue.length == 0 || checkEmpty(inputValue)) {
+        alert("Te rog introdu o intrebare valida.");
+        return;
+    }
+
+    updateJsonData("intrebari", inputValue);
+
+}
+
+
+function updateJsonData(key, value) {
+    // fetch json data
+    let jsonData;
+
+    fetch(jsonPath).then(
+        response => { return response.json(); }
+    ).then(
+        data => {
+            console.log(data);
+            data.intrebari.push("afuihi");
+            console.log(data);
+        }
+    )
+
+    // fetch(jsonPath).then(
+    //     response => {return response.json();}
+    // ).then(
+    //     data => {
+    //         console.log(data);
+    //     }
+    // )
+    
+    
 }
